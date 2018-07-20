77e967fc8859250b679e70efcb4bcb51742dbed7
Daigo HIROOKA <tappy.hello-planet@ezweb.ne.jp>
2018-07-21 02:59:56 +0900

    add answers

---
+++ AtCoder/python/ABC049/ProbB.py
@@ -0,0 +1,6 @@
+h, w = list(map(int, input().split()))
+c = [input() for _ in range(h)]
+# print(c)
+for i in range(h):
+    print(c[i])
+    print(c[i])
---
+++ AtCoder/python/ABC049/ProbC.py
@@ -0,0 +1,19 @@
+s = input()
+
+def solve(s):
+    while len(s) > 0:
+        if s[-5:] == 'dream':
+            s = s[:-5]
+        elif s[-7:] == 'dreamer':
+            s = s[:-7]
+        elif s[-5:] == 'erase':
+            s = s[:-5]
+        elif s[-6:] == 'eraser':
+            s = s[:-6]
+        else:
+            print('NO')
+            return 
+    print('YES')
+    return
+
+solve(s)
